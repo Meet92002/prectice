9ec6b051217bea375992f6d35d50f8a16a6755ee
Meet92002 <meet92002@gmail.com>
2023-06-27 19:04:40 +0530

    blure with slider num

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -8,10 +9,12 @@ import 'model/images_model.dart';
 
 class HomeScreen extends StatefulWidget {
    String? title;
+   int? id;
    ImageListModel? imagedata;
    HomeScreen({
     @required this.title,
      @required this.imagedata,
+     @required this.id,
   });
 
   @override
@@ -20,7 +23,7 @@ class HomeScreen extends StatefulWidget {
 List<ImageListModel?> imagelist = [];
 class _HomeScreenState extends State<HomeScreen> {
 
-  double blurImage = 0;
+  double blurImage=0;
   double sigmaX = 0;
   double sigmaY = 0;
   int num = 1;
@@ -43,10 +46,21 @@ class _HomeScreenState extends State<HomeScreen> {
             children: [
               buildBlurredImage(),
               const SizedBox(height:0),
+              // Slider(
+              //   value: blurImage,
+              //   max: 10,
+              //   onChanged: (value) => setState(() => blurImage = value),
+              // ),
               Slider(
                 value: blurImage,
                 max: 10,
-                onChanged: (value) => setState(() => blurImage = value),
+                divisions: 5,
+                label: blurImage.round().toString(),
+                onChanged: (double value) {
+                  setState(() {
+                    blurImage = value;
+                  });
+                },
               ),
               ElevatedButton(
                 child: Text('Next image'),
@@ -68,20 +82,21 @@ class _HomeScreenState extends State<HomeScreen> {
         Container(
           height: 600,
           width: 450,
-          child: Image.network(widget.imagedata!.downloadUrl.toString(),
-            // 'https://picsum.photos/id/25/200/300/?blur=$blurImage&$id&$num',
+          child: Image.network(
+            // widget.imagedata!.downloadUrl.toString(),
+            'https://picsum.photos/id/${widget.imagedata!.id}/200/300/?blur=$blurImage&$num',
             fit: BoxFit.cover,
           ),
         ),
-        Positioned.fill(
-          child: BackdropFilter(
-            filter: ImageFilter.blur(
-              sigmaX: blurImage,
-              sigmaY: blurImage,
-            ),
-            child: Container(color: Colors.black.withOpacity(0.2)),
-          ),
-        ),
+        // Positioned.fill(
+        //   child: BackdropFilter(
+        //     filter: ImageFilter.blur(
+        //       sigmaX: blurImage,
+        //       sigmaY: blurImage,
+        //     ),
+        //     child: Container(color: Colors.black.withOpacity(0.2)),
+        //   ),
+        // ),
       ],
     ),
   );
